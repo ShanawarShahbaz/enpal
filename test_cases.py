d1882e4effcedd94ed7ee8a9fe2d4fac927f7949
ShanawarShahbaz <shanawar.shahbaz6@gmail.com>
2021-04-30 14:59:08 +0200

    Update test_cases.py
    Test case 1: after seperation of function definations

--- test_cases.py
+++ test_cases.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 import time
 import os
+import enpal_defination as enp
 PATH=("C:\Program Files (x86)\chromedriver.exe")
 driver=webdriver.Chrome(PATH)
 cwd_path=os.getcwd()
@@ -14,113 +15,68 @@ print(">>>>>>>",cwd_path,"<<<<<<<")
 # create action chain object
 action = ActionChains(driver)
 
-def ja():
-    #Test Case -Nein
-    time.sleep(1)
-    arrow = driver.find_element_by_xpath('//div[@data-testid="label-container" and @class="SingleAnswer_AnswerLabelContainer__316pD"]')
-    arrow.click()
 
-def roof_type(path):
-    arrow = driver.find_element_by_xpath('//div[@data-testid="label-container" and @class="SingleAnswer_AnswerLabelContainer__316pD"]')
-    arrow.click()
-
-def homepage():
-    driver.get("https://dynamic-slider-staging.azurewebsites.net/")
-    print("site successfully loaded!")
-    time.sleep(1)
-
-def family_size(path):
-    time.sleep(1)
-    arrow = driver.find_element_by_xpath(path)
-    arrow.click()
-
-def daytime(path):
-    time.sleep(1)
-    arrow = driver.find_element_by_xpath(path)
-    arrow.click()
-
-def house_owner(path):
-    time.sleep(1)
-    arrow = driver.find_element_by_xpath(path)
-    arrow.click()
-
-def fill_form(path,keys):
-    arrow=driver.find_element_by_xpath(path)
-    arrow.send_keys(keys)
-    
-def age_check(path):
-    time.sleep(1)
-    arrow = driver.find_element_by_xpath(path)
-    arrow.click()
-
-def submit_button(path):
-    arrow=driver.find_element_by_xpath(path)
-    arrow.send_keys(Keys.ENTER)
-
-def click(path):
-    arrow=driver.find_element_by_xpath(path)
-    arrow.click()
 
 #Test Case-1 - Sanity Check - Loading the dynamic0slider-staging.azurewebsites.net
-homepage() 
+enp.homepage() 
 
 # Test Case -Navigation -Accessing the roof type SattleDach
 roof_path=arrow ='//div[@data-testid="label-container" and @class="SingleAnswer_AnswerLabelContainer__316pD"]'
-roof_type(roof_path)
+enp.roof_type(roof_path)
 #sattelDach()
 
 #Test Case -JA
-ja()
+enp.ja()
 
 #Test Case 1-2
 family_path='//div[@data-testid="label-container" and @class="SingleAnswer_AnswerLabelContainer__316pD"]'
-family_size(family_path)
+enp.family_size(family_path)
 #--------------
 time_path='//div[@data-testid="answer" and @class="SingleAnswer_Answer__sWhX8"]'
-daytime(time_path)
+enp.daytime(time_path)
 #----house owner-- ? JA or Nein ->Nein
 house_path='((//DIV[@data-testid="label-container"])[1]/../../..//DIV[@data-testid="label-container"])[2]'
-house_owner(house_path)
+enp.house_owner(house_path)
  
 #---zip_code()
 time.sleep(2)
 zip_path='//INPUT[@name="zipCode"]'
 zip_number="12345"
-fill_form(zip_path,zip_number)
+enp.fill_form(zip_path,zip_number)
 
 # ---Press Next/Submit/Weiter
 zip_path='//BUTTON[@data-testid="submit"][text()="Weiter"]'
-submit_button(zip_path)
+enp.submit_button(zip_path)
 
 #--wiriting name----------------
 time.sleep(5)
 name_path='//input[@name="name"]'
 name_x='abc 123'
-fill_form(name_path,name_x)
+enp.fill_form(name_path,name_x)
 
 #----- writting address------
 address_path='//input[@name="address"]'
 address_x="alexnder plazt strasse 23 12581 berlin"
-fill_form(address_path,address_x)
+enp.fill_form(address_path,address_x)
 
 #------writing phone number
 phone_path='//input[@name="phone"]'
 phone_number="+491797266549"
-fill_form(phone_path,phone_number)
+enp.fill_form(phone_path,phone_number)
 
 #--writing email -----
 email_path='//input[@name="email"]' 
 email_address="shan@mail.com"
-fill_form(email_path,email_address)
+enp.fill_form(email_path,email_address)
 
 #---------Submit Contact details---
 contact_form_button_path='//BUTTON[@data-testid="submit"]'
-submit_button(contact_form_button_path)
+enp.submit_button(contact_form_button_path)
 
 #-----wait for script to run to submit contact details form----
 time.sleep(4)
 weiter_path='//BUTTON[@data-testid="submit"][text()="Weiter"]'
-submit_button(weiter_path)
+enp.submit_button(weiter_path)
 
 #--How many household are registers -initally click on 3 , to check the error message, then it will click on 2?-----------------------
 time.sleep(1)
@@ -141,11 +97,11 @@ arrow.click()
 #check : are you 70 or under 70 ?
 #--above_70()
 age_path='(//DIV[@data-testid="label-container"])[2]'
-age_check(age_path)
+enp.age_check(age_path)
 
 #--below_70()
 age_path='(//DIV[@data-testid="label-container"])[1]'
-age_check(age_path)
+enp.age_check(age_path)
 
 #---Description about house 250 lenth characters ---
 text_house_description=''' When the days are cold
@@ -162,52 +118,36 @@ This is my kingdom come  This is my kingdom come.This is my king'''
 time.sleep(2)
 house_path='//TEXTAREA[@name="freetext"]'
 house_desc=text_house_description
-fill_form(house_path,house_desc)
-
-submit_button('//BUTTON[@data-testid="freetext-submit"]')
-
-#//INPUT[@id='reminder-input']
+enp.fill_form(house_path,house_desc)
+#---submit house description
+enp.submit_button('//BUTTON[@data-testid="freetext-submit"]')
 
 #----goto-upload picures
 time.sleep(1)
 goto_picuters_path='//BUTTON[@data-testid="submit"]'
-submit_button(goto_picuters_path)
+enp.submit_button(goto_picuters_path)
 
-#-----Upload picture button
-#//*[@id="container_image-roof"]
-#/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div/div[3]/div/form/input
-#to identify element
-def upload_picture(element_path,local_picture_path):
-    s = driver.find_element_by_xpath(element_path)
-    s.send_keys(local_picture_path)
-
+#--start uploading pictueing from local dir
 element_path="/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div/div[3]/div/form/input[@type='file']"
 picture_path_1="D:\\enpal\\a.jpg"
 picture_path_2="D:\\enpal\\b.png"
 picture_path_3="D:\\enpal\\script.sql"
-upload_picture(element_path,picture_path_1)
-upload_picture(element_path,picture_path_2)
-upload_picture(element_path,picture_path_2)
-upload_picture(element_path,picture_path_1)
-upload_picture(element_path,picture_path_2)
-upload_picture(element_path,picture_path_2)
-upload_picture(element_path,picture_path_1)
-upload_picture(element_path,picture_path_2)
+enp.upload_picture(element_path,picture_path_1)
+enp.upload_picture(element_path,picture_path_2)
+enp.upload_picture(element_path,picture_path_2)
+enp.upload_picture(element_path,picture_path_1)
+enp.upload_picture(element_path,picture_path_2)
+enp.upload_picture(element_path,picture_path_2)
+enp.upload_picture(element_path,picture_path_1)
+enp.upload_picture(element_path,picture_path_2)
 
-upload_picture(element_path,picture_path_3)
-
-
-#/html/body/div/div[1]/div[2]/ol/li/a
-#//A[@data-testid='next-step']
-#phone_reminder_path='//INPUT[@id="reminder-input"]'
-#phone_number_reminder="1797266549"
-# fill_form(phone_reminder_path,phone_number_reminder)
+enp.upload_picture(element_path,picture_path_3)
 
 #---submit phone number for reminder
 time.sleep(3)
-submit_button('//BUTTON[@type="submit"]')
+enp.submit_button('//BUTTON[@type="submit"]')
 #time.sleep(2)
-click('//A[@data-testid="next-step"]')
+enp.click('//A[@data-testid="next-step"]')
 
 #---House with surrouding----
 
@@ -215,5 +155,31 @@ element_path="/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div/div[3]/div/form
 picture_path_1="D:\\enpal\\a.jpg"
 picture_path_2="D:\\enpal\\b.png"
 picture_path_3="D:\\enpal\\script.sql"
-upload_picture(element_path,picture_path_1)
-upload_picture(element_path,picture_path_2)
+enp.upload_picture(element_path,picture_path_1)
+enp.upload_picture(element_path,picture_path_2)
+#---Next button for house surrounding
+enp.click('//A[@data-testid="next-step"]')
+
+#------meter picure---------------
+element_path="/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div/div[3]/div/form/input"
+picture_path_1="D:\\enpal\\a.jpg"
+picture_path_2="D:\\enpal\\b.png"
+picture_path_3="D:\\enpal\\script.sql"
+enp.upload_picture(element_path,picture_path_1)
+enp.upload_picture(element_path,picture_path_2)
+time.sleep(1)
+enp.click('//A[@data-testid="next-step"]')
+#------electric city bill pictures-------
+element_path="/html/body/div[1]/div[1]/div[1]/div/div/div[2]/div/div[3]/div/form/input"
+picture_path_1="D:\\enpal\\a.jpg"
+picture_path_2="D:\\enpal\\b.png"
+picture_path_3="D:\\enpal\\script.sql"
+enp.upload_picture(element_path,picture_path_1)
+enp.upload_picture(element_path,picture_path_2)
+enp.click('//A[@data-testid="next-step"]')
+
+#-------Finish Button---------------------
+time.sleep(5)
+element_path="/html/body/div/div[1]/div[1]/div/div/div[2]/div/div[3]/button"
+enp.submit_button(element_path)
+
